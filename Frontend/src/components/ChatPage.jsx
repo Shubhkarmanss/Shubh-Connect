68036a7e885b37fb8d37eef69fda91cbc715e521
Shubhkarmanss <shubhkarmansabharwal@gmail.com>
2025-07-22 16:20:10 +0530

    back to shubh-connect url

--- Frontend/src/components/ChatPage.jsx
+++ Frontend/src/components/ChatPage.jsx
@@ -17,7 +17,7 @@ const ChatPage = () => {
 
     const sendMessageHandler = async (receiverId) => {
         try {
-            const res = await axios.post(`https://connectkaro.onrender.com/api/v1/message/send/${receiverId}`, { textMessage }, {
+            const res = await axios.post(`https://shubh-connect.onrender.com/api/v1/message/send/${receiverId}`, { textMessage }, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post('https://connectkaro.onrender.com/api/v1/user/login', input, {
+            const res = await axios.post('https://shubh-connect.onrender.com/api/v1/user/login', input, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
